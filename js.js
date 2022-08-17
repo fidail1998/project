a9ebf89ba24b4fc001bbccb7622484cecddc9867
Fidail <fipya@mail.ru>
2022-08-17 13:31:21 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,45 @@
+// "use strict"
+// a = 15
+// console.log(a)
+// {
+//     var result = 50;
+// }
+// console.log(result);
+
+// console.log([].push('a'))
+
+// let name = `${a}`
+// console.log(name * 1)
+
+// for (let i = 1; i <= 100; i++) { 
+//     if (i % 5 == 0 && i % 3 == 0) {
+//         document.write('<br>' + i + ':' + 'foobar' + '</br>')
+//     } else  if (i % 5 == 0) {
+//         document.write('<br>' + i + ':' + 'foo' + '</br>')
+//         } else if (i % 3 == 0 ) {
+//             document.write('<br>' + i + ':' + 'bar' + '</br>')
+//             }
+//         } eqrqew
+    
+
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+// alert('hello')
+
+// let answer = +prompt('вам есть 18?', 'yes')
+// console.log((answer + 5))
+
+// let arr = []
+// arr[0] = '1'
+// arr[1] = 'p2'
+// arr[2] = 'p3'
+// console.log(typeof(arr))
+
+// const category = 'toys'
+// console.log(`https://someurl.com/${category}`)
+// let a = 10,
+// b = 10;
+// console.log(a++)
+// console.log(++b)
+
+console.log(!false)
